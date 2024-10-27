9b3977e40eea0ea3d0ca2f14cc46daeed0a1196b
GTimothy <22472919+GTimothy@users.noreply.github.com>
2024-10-27 16:06:19 +0100

    experimenting with tryfrom implementation

--- rust/kernel/bitflag.rs
+++ rust/kernel/bitflag.rs
@@ -15,6 +15,7 @@ pub trait BitFlag {
 }
 
 /// A BitFlag's bits' wrapper with build constraints.
+#[derive(Debug, PartialEq)]
 pub struct ConstrainedFlag<T: BitFlag>(T::Bits);
 
 /// A constrained flag's builder implements a build function once it reaches a valid state.
@@ -70,4 +71,175 @@ fn simple_size_colour_bitflag() {
     let flag_builder = ConstrainedFlag::<CustomBitFlag>::builder();
     let flag = flag_builder.with_big().with_red().build();
     assert_eq!(flag.0, BIG | RED);
+}
+
+#[test]
+fn test2() {
+    use crate::bitflag::{bitflag, ConstrainedFlag};
+    const BIG: u32 = 0u32;
+    const SMALL: u32 = 1u32;
+
+    const RED: u32 = 2u32;
+    const GREEN: u32 = 4u32;
+    const BLUE: u32 = 7u32;
+
+    #[derive(Debug, PartialEq)]
+    pub struct CustomBitFlag;
+    impl BitFlag for CustomBitFlag {
+        type Bits = u32;
+    }
+
+    impl crate::bitflag::ConstrainedFlag<CustomBitFlag> {
+        pub fn builder(
+        ) -> CustomBitFlagBuilder<crate::bitflag::Missing<Size>, crate::bitflag::Missing<Colour>>
+        {
+            CustomBitFlagBuilder {
+                flags: Default::default(),
+                t: core::marker::PhantomData,
+            }
+        }
+    }
+
+    #[derive(Debug)]
+    #[repr(C)]
+    pub struct CustomBitFlagBuilder<S0, S1> {
+        flags: [Option<u32>; 2],
+        t: core::marker::PhantomData<(S0, S1)>,
+    }
+
+    #[derive(Debug)]
+    pub struct Size;
+
+    #[derive(Debug)]
+    pub struct Colour;
+
+    impl<S1> CustomBitFlagBuilder<crate::bitflag::Valid<Size>, S1> {
+        pub fn set_big(&mut self) {
+            self.flags[0] = Some(BIG);
+        }
+
+        pub fn set_small(&mut self) {
+            self.flags[0] = Some(SMALL);
+        }
+    }
+
+    impl<S0> CustomBitFlagBuilder<S0, crate::bitflag::Valid<Colour>> {
+        pub fn set_red(&mut self) {
+            self.flags[1] = Some(RED);
+        }
+
+        pub fn set_green(&mut self) {
+            self.flags[1] = Some(GREEN);
+        }
+
+        pub fn set_blue(&mut self) {
+            self.flags[1] = Some(BLUE);
+        }
+    }
+
+    impl<S1> CustomBitFlagBuilder<crate::bitflag::Missing<Size>, S1> {
+        pub fn with_big(self) -> CustomBitFlagBuilder<crate::bitflag::Valid<Size>, S1> {
+            let mut b: CustomBitFlagBuilder<crate::bitflag::Valid<Size>, S1> =
+                unsafe { core::mem::transmute(self) };
+            b.set_big();
+            b
+        }
+
+        pub fn with_small(self) -> CustomBitFlagBuilder<crate::bitflag::Valid<Size>, S1> {
+            let mut b: CustomBitFlagBuilder<crate::bitflag::Valid<Size>, S1> =
+                unsafe { core::mem::transmute(self) };
+            b.set_small();
+            b
+        }
+    }
+
+    impl<S0> CustomBitFlagBuilder<S0, crate::bitflag::Missing<Colour>> {
+        pub fn with_red(self) -> CustomBitFlagBuilder<S0, crate::bitflag::Valid<Colour>> {
+            let mut b: CustomBitFlagBuilder<S0, crate::bitflag::Valid<Colour>> =
+                unsafe { core::mem::transmute(self) };
+            b.set_red();
+            b
+        }
+
+        pub fn with_green(self) -> CustomBitFlagBuilder<S0, crate::bitflag::Valid<Colour>> {
+            let mut b: CustomBitFlagBuilder<S0, crate::bitflag::Valid<Colour>> =
+                unsafe { core::mem::transmute(self) };
+            b.set_green();
+            b
+        }
+
+        pub fn with_blue(self) -> CustomBitFlagBuilder<S0, crate::bitflag::Valid<Colour>> {
+            let mut b: CustomBitFlagBuilder<S0, crate::bitflag::Valid<Colour>> =
+                unsafe { core::mem::transmute(self) };
+            b.set_blue();
+            b
+        }
+    }
+
+    impl crate::bitflag::ConstrainedFlagBuilder<CustomBitFlag>
+        for CustomBitFlagBuilder<crate::bitflag::Valid<Size>, crate::bitflag::Valid<Colour>>
+    {
+        fn build(self) -> crate::bitflag::ConstrainedFlag<CustomBitFlag> {
+            crate::bitflag::ConstrainedFlag::<CustomBitFlag>(
+                self.flags.iter().flatten().sum::<u32>(),
+            )
+        }
+    }
+
+    impl TryFrom<<CustomBitFlag as BitFlag>::Bits> for ConstrainedFlag<CustomBitFlag> {
+        type Error = u32;
+
+        fn try_from(value: <CustomBitFlag as BitFlag>::Bits) -> Result<Self, Self::Error> {
+            let builder = Self::builder();
+            let mut to_process: u32 = value.clone();
+
+            let mut matched = false;
+
+            for flag in [BIG, SMALL] {
+                if (flag & to_process) == flag {
+                    matched = true;
+                    to_process -= flag;
+                    if flag > 0 {
+                        break;
+                    }
+                }
+            }
+            if !matched {
+                return Err(value);
+            }
+            matched = false;
+
+            for flag in [RED, BLUE, GREEN] {
+                if (flag & to_process) == flag {
+                    to_process -= flag;
+                    matched = true;
+                    if flag > 0 {
+                        break;
+                    }
+                }
+            }
+            if matched && to_process == 0 {
+                return Ok(Self(value));
+            }
+
+            // Error("algorithm could not deduce valid flag combination.")
+            Err(value)
+        }
+    }
+
+    ConstrainedFlag::<CustomBitFlag>::builder()
+        .with_big()
+        .with_red()
+        .build();
+
+    let res: Result<ConstrainedFlag<CustomBitFlag>, u32> = 1u32.try_into();
+    assert_eq!(res, Err(1), "1u should fail");
+    let res: Result<ConstrainedFlag<CustomBitFlag>, u32> = (RED | BLUE).try_into();
+    assert_eq!(res, Err(RED | BLUE), "RED BLUE should fail");
+    let res: Result<ConstrainedFlag<CustomBitFlag>, u32> = (SMALL | GREEN).try_into();
+    assert_eq!(
+        res,
+        Ok(ConstrainedFlag(SMALL | GREEN)),
+        "Small Green should succeed"
+    );
 }
